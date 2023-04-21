b741ad59312a9022af8df802fe2d143411b47b67
joshuablackmore <106779167+joshuablackmore@users.noreply.github.com>
2023-04-21 13:45:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          come on, this is getting annoying now.
         </p>
         <a
           className="App-link"
