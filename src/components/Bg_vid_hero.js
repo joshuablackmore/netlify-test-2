e0333b1df073830e3f849f5c062172fb26939477
joshuablackmore <106779167+joshuablackmore@users.noreply.github.com>
2023-04-22 22:04:36 +0100

    dvh to screen sizes

--- src/components/Bg_vid_hero.js
+++ src/components/Bg_vid_hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BackgroundHero = () => {
   return (
-    <div className="relative flex w-full h-[100dvh] items-center justify-center  min-[466px]:h-screen sm:h-screen md:h-screen lg:h-screen xl:h-screen overflow-hidden">
+    <div className="relative flex w-full h-[100dvh] items-center justify-center  min-[466px]:h-[100dvh] sm:h-[100dvh] md:h-[100dvh] lg:h-[100dvh] xl:h-[100dvh] overflow-hidden">
         <video
           autoPlay
           playsInline
