fb659cf3cfd41544294baba49dd1c80cd69cfce5
joshuablackmore <106779167+joshuablackmore@users.noreply.github.com>
2023-04-24 13:10:01 +0100

    replaced favicon file

--- src/components/Bg_vid_hero.js
+++ src/components/Bg_vid_hero.js
@@ -17,7 +17,7 @@ const BackgroundHero = () => {
         <div className='text-white z-10'>
     <h1 className='text-5xl text-center leading-normal'>joshuablackmore.com</h1>
     <h1 className='text-4xl text-center leading-normal'>under construction</h1>
-    <h2 className="flex pt-5 items-center justify-center text-center text-2xl font-bold">Watch some<a href="https://www.youtube.com/watch?v=z_s1Nb3JwDY&ab_channel=JoshuaBlackmore" className='text-yellow-300 text-4xl hover:text-5xl hover:text-pink-500 px-2 transition-all'>video</a>for now</h2>
+    <h2 className="flex pt-5 items-center justify-center text-center text-2xl font-bold">You can watch some<a href="https://www.youtube.com/watch?v=z_s1Nb3JwDY&ab_channel=JoshuaBlackmore" className='text-yellow-300 text-4xl hover:text-5xl hover:text-pink-500 px-2 transition-all'>video</a>for now</h2>
     </div>
     </div>
     </div>
