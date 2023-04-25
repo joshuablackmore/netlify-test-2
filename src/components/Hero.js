e5c8e0c9b35af631a7f39dee271d312ea020d298
joshuablackmore <106779167+joshuablackmore@users.noreply.github.com>
2023-04-25 11:09:34 +0100

    fixed text flex wrap thing

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,10 @@ const Hero = () => {
         <div className='absolute top-0 bottom-0 left-0 right-0 bg-black/70 h-[100dvh] overflow-hidden' />
         <div className='text-white z-10'>
         <h1 className='text-5xl text-center'>Site under construction</h1>
-        <h2 className="flex pt-5 items-center justify-center text-center text-2xl font-bold">Watch some<a href="#videos" className='text-yellow-300 text-4xl hover:text-pink-500 px-2'>videos</a>for now</h2>
+          <div className='flex flex-wrap text-center items-center justify-center'>
+            <h2 className="pt-5 text-2xl">Here's some<a href="#videos" className='text-yellow-300 font-bold text-3xl hover:text-pink-500 px-2'>videos</a>for now</h2>
+          </div>
+
         </div>
     </div>
   )
